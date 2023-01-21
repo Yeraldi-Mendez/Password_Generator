dfedb7f1613cec2bde44112bb81e9d87dfdc1565
Yeraldi-Mendez <yeraldimendez.ym@gmail.com>
2023-01-20 18:59:09 -0800

    added the second step to if statements along with random password generator.. did not seem to work

--- Develop/script.js
+++ Develop/script.js
@@ -22,36 +22,70 @@ var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n
 var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
 //confirmation variables
-var passwordLength;
+var passwordLength; 
 var confirmNumber;
 var confirmSchar;
 var confirmLower;
 var confirmUpper;
+var selectedCriteria;
 
 //start function to ask user's input
+// this are if they answer all 
 function generatePassword(){
   passwordLength = prompt("How many characters would your password contain? (8-128)");
   console.log("Password Lenght " + passwordLength);
-
+  //user did not respond to password length
   if(!passwordLength){
     alert("Required value");
   }
-
+  //user selects length less than 8 character or over 128
   else if (passwordLength < 8 || passwordLength > 128){
     passwordLength = prompt("Password must be between 8 and 128 characters in length");
     console.log("Password length " + passwordLength);
   }
-
+  //user selects password length between 8 and 128
   else {
-    confirmNumber = confirm ("Will your password contain numbers?");
+    confirmNumber = confirm("Will your password contain numbers?");
     console.log("Number " + confirmNumber);
-    confirmNumber = confirm ("Will your password contain special characters?");
+    confirmSchar = confirm("Will your password contain special characters?");
     console.log("Special Character " + confirmSchar);
-    confirmNumber = confirm ("Will your password contain lower case letters?");
+    confirmLower = confirm("Will your password contain lower case letters?");
     console.log("Lower case " + confirmLower);
-    confirmNumber = confirm ("Will your password contain upper case letters?");
+    confirmUpper = confirm("Will your password contain upper case letters?");
     console.log("Upper case " + confirmUpper);
   };
 
-  
+  //these is to continue user's input but some or all have no answer
+  //user selects no criteria
+  if(!confirmNumber && !confirmSchar && !confirmLower && !confirmUpper){
+    alert("You must select at least one criteria");    
+  } 
+  else {
+    if(confirmNumber){
+      selectedCriteria = selectedCriteria.concat(number)
+    } 
+
+    if(confirmSchar){
+      selectedCriteria = selectedCriteria.concat(sChar)
+    }
+
+    if(confirmLower){
+      selectedCriteria = selectedCriteria.concat(lower)
+    }
+
+    if(confirmUpper){
+      selectedCriteria = selectedCriteria.concat(upper)
+    }
+  }
+
+  console.log(selectedCriteria)
+
+  var ranPassword = [];
+
+  for (var i = 0; i < passwordLength; i++){
+    ranPassword = ranPassword + selectedCriteria[Math.floor(math.random () * selectedCriteria.length)];
+    console.log(ranPassword)
+  }
+
+  return ranPassword;
 }
